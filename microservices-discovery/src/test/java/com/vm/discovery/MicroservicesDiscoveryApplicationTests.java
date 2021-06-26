cf070c5dca2277b2f1d29d32b7eeffdf316d344c
Sai Teja <Sai Teja@DESKTOP-N2VQBHK>
2021-06-26 16:31:36 +0530

    initial commit

---
+++ microservices-discovery/src/test/java/com/vm/discovery/MicroservicesDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vm.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
